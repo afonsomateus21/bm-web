77c71bb5cf7593cb0043e4194246e049084fe465
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-16 23:08:44 -0300

    feat: atualiza exportacoes dos componentes de botao

--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,2 +1,4 @@
 export { FlatButton } from "./FlatButton"
 export { IconButton } from "./IconButton"
+export { PreviousPageButton } from "./PreviousPageButton";
+export { HomeButton } from "./HomeButton";
