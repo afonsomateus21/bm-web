402658685e0fbe2d7e8243b717a85ee6ede81005
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-20 18:25:47 -0300

    feat: cria select customizado

---
+++ src/components/inputs/CustomSelect.tsx
@@ -0,0 +1,45 @@
+import { CustomSelectProps } from "../../types/custom-select-props";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+import { useState } from "react";
+
+export function CustomSelect({ 
+  title, errors, options, required = false
+}: CustomSelectProps) {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="w-full max-w-sm flex flex-col">
+      <label 
+        htmlFor={title.split(' ')[0].toLowerCase()} 
+        className="text-tertiary font-bold flex items-center gap-1"
+      >
+        {title} {required && <span className="text-red-500 text-2xl leading-none">*</span>}
+      </label>
+        <Listbox value={selected} onChange={ setSelected }>
+          <div className="relative">
+            {/* Botão que abre o dropdown */}
+            <ListboxButton className="w-full h-12 bg-white border-2 border-secondary rounded-4xl px-4 flex justify-between items-center">
+              <span>{ selected.label }</span>
+              <ArrowDropDownIcon htmlColor="black" fontSize="medium" />
+            </ListboxButton>
+
+            {/* Dropdown (a div que aparece ao clicar) */}
+            <ListboxOptions className="absolute w-full mt-2 bg-white border border-gray-300 rounded-md shadow-lg z-10">
+              {options.map((option) => (
+                <ListboxOption 
+                  key={option.value} 
+                  value={option} 
+                  className="cursor-pointer px-4 py-2 hover:bg-gray-100"
+                >
+                  {option.label}
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </div>
+        </Listbox>
+      <p className="text-red-500"> {errors} </p>
+    </div>
+  );
+}
+
