acfbc50c339aeddfd489f74166d8d74c86e290c5
claralmoura <claramoura2303@gmail.com>
2025-02-24 07:41:56 -0300

    ✨ | feat(service-management): adiciona schema de validação

--- src/utils/validations/index.ts
+++ src/utils/validations/index.ts
@@ -1,3 +1,4 @@
 export { loginSchema } from "./loginSchema";
 export { registerSchema } from "./registerSchema";
 export { registerServiceSchema } from "./registerServiceSchema";
+export { registerProfessionalSchema } from "./registerProfessionalSchema";
