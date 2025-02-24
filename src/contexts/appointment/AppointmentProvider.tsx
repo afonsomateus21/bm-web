c3d991e616c9a77163c2840bd710ab6bf47a39ea
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 01:33:25 -0300

    feat: adiciona funcao para listar os profissionais

--- src/contexts/appointment/AppointmentProvider.tsx
+++ src/contexts/appointment/AppointmentProvider.tsx
@@ -1,17 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { AppointmentContext } from "./AppointmentContext"
-import { Appointment, AppointmentFormInput, CustomProviderProps } from "../../types"
+import { Appointment, AppointmentFormInput, CustomProviderProps, User } from "../../types"
 import { api } from "../../services";
+import { useAuth } from "../../hooks";
 
 export const AppointmentProvider = ({ children }: CustomProviderProps) => {
   const [ appointment, setAppointment ] = useState<Appointment | null>();
+  const [ professionals, setProfessionals ] = useState<{ label: string; value: string }[]>([]);
+  
+  const { accessToken } = useAuth();
+
+  useEffect(() => {
+    const fetchProfessionals = async () => {
+      const response = await getProfessionals();
+      setProfessionals(response);
+    }
+
+    fetchProfessionals();
+  }, []);
+
+
 
   async function createAppointment(appointmentInput: AppointmentFormInput) {
     try {
       const appointment = {
-        "professional_id": appointmentInput.professionalId,
-        "customer_id": appointmentInput.customerId,
-        "service_id": appointmentInput.serviceId,
+        "professional_id": appointmentInput.professional,
+        "customer_id": appointmentInput.customer,
+        "service_id": appointmentInput.service,
         "date": appointmentInput.date,
         "hour": appointmentInput.hour,
         "is_notifiable": appointmentInput.isNotifiable
@@ -24,8 +39,29 @@ export const AppointmentProvider = ({ children }: CustomProviderProps) => {
     } 
   }
 
+  async function getProfessionals() {
+    const response = await api.get(
+      "/auth/user/admin",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${accessToken}`
+        }
+      }
+    );
+
+    const professionals = response.data ? response.data : [];
+
+    console.log(professionals);
+
+    return professionals.map(({ id, first_name, last_name }: { id: string; first_name: string; last_name: string }) => ({
+      label: `${first_name} ${last_name}`,
+      value: id! 
+    }));
+  }
+
   return (
-    <AppointmentContext.Provider value={{ appointment, createAppointment }}>
+    <AppointmentContext.Provider value={{ appointment, professionals, createAppointment }}>
       { children }
     </AppointmentContext.Provider>
   )
