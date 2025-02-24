545fed0b47a196471b1eb559ece0041901a22f5f
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 02:06:59 -0300

    feat: adiciona funcao para recuperar servico de acordo com o profissional

--- src/contexts/appointment/AppointmentProvider.tsx
+++ src/contexts/appointment/AppointmentProvider.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { AppointmentContext } from "./AppointmentContext"
-import { Appointment, AppointmentFormInput, CustomProviderProps, User } from "../../types"
+import { Appointment, AppointmentFormInput, CustomProviderProps } from "../../types"
 import { api } from "../../services";
 import { useAuth } from "../../hooks";
 
@@ -18,8 +18,6 @@ export const AppointmentProvider = ({ children }: CustomProviderProps) => {
 
     fetchProfessionals();
   }, []);
-
-
 
   async function createAppointment(appointmentInput: AppointmentFormInput) {
     try {
@@ -52,16 +50,38 @@ export const AppointmentProvider = ({ children }: CustomProviderProps) => {
 
     const professionals = response.data ? response.data : [];
 
-    console.log(professionals);
-
     return professionals.map(({ id, first_name, last_name }: { id: string; first_name: string; last_name: string }) => ({
       label: `${first_name} ${last_name}`,
       value: id! 
     }));
   }
 
+  async function getServicesByProfessional(professionalId: string) {
+    const response = await api.get(
+      "/service/professional",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${accessToken}`
+        },
+        params: {
+          "professional_id": professionalId
+        }
+      }
+    );
+
+    console.log(response);
+
+    const services = response.data ? response.data : [];
+
+    return services.map(({ id, title }: { id: string; title: string; }) => ({
+      label: title,
+      value: id! 
+    }));
+  }
+
   return (
-    <AppointmentContext.Provider value={{ appointment, professionals, createAppointment }}>
+    <AppointmentContext.Provider value={{ appointment, professionals, createAppointment, getServicesByProfessional }}>
       { children }
     </AppointmentContext.Provider>
   )
