dbe3cc9338e600f641dee524abb0b330ae54fe16
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 12:00:34 -0300

    feat: atualiza exportacoes dos inputs

--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -2,3 +2,4 @@ export { FormInput } from "./FormInput"
 export { PhotoInput } from "./PhotoInput";
 export { CustomSelect } from "./CustomSelect";
 export { DateInput } from "./DateInput";
+export { SearchDateInput } from "./SearchDateInput";
