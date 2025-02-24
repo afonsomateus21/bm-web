072c76f0e4a12307668357ecc96c31cc06a47e03
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 11:57:12 -0300

    refac: torna o campo titulo opcional

--- src/types/input-props.ts
+++ src/types/input-props.ts
@@ -1,7 +1,7 @@
 import { InputHTMLAttributes, ReactNode } from "react";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  title: string;
+  title?: string;
   required?: boolean;
   icon?: ReactNode | string;
   errors?: string | undefined;
