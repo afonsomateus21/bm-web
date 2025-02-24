6ac3d2d9e5415ea44a4e0dd37e3b2383266ae4cb
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 02:50:51 -0300

    feat: adiciona o AppointmentProvider no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { RouterProvider } from 'react-router';
 import './index.css'
 import { router } from './routes';
-import { AuthProvider, PasswordProvider } from './contexts';
+import { AppointmentProvider, AuthProvider, PasswordProvider } from './contexts';
 import './i18n.ts';
 
 function App() {
   return (
     <div className='bg-primary h-screen'>
       <AuthProvider>
-        <PasswordProvider>
-          <RouterProvider router={ router } />
-        </PasswordProvider>
+        <AppointmentProvider>
+          <PasswordProvider>
+            <RouterProvider router={ router } />
+          </PasswordProvider>
+        </AppointmentProvider>
       </AuthProvider>
     </div>
   );
