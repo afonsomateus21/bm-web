41fee8ed288113a714b332294e543efd61dcf68a
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 15:36:25 -0300

    feat: adiciona customer no schema

--- src/utils/validations/appointmentSchema.tsx
+++ src/utils/validations/appointmentSchema.tsx
@@ -1,10 +1,15 @@
 import { TFunction } from "i18next";
 import { object, string, boolean, date } from "yup";
 
-export const appointmentSchema = (t: TFunction) =>
+export const appointmentSchema = (t: TFunction, isReservation: boolean) =>
   object({
     professional: string()
       .required(t("Common.Form.Errors.Professional.Required")),
+    customer: string().when([], {
+      is: () => isReservation,
+      then: schema => schema.required(t("Common.Form.Errors.Customer.Required")),
+      otherwise: schema => schema.optional()
+    }),
     service: string()
       .required(t("Common.Form.Errors.Service.Required")),
     date: date()
@@ -13,4 +18,4 @@ export const appointmentSchema = (t: TFunction) =>
       .required(t("Common.Form.Errors.Hour.Required")),
     isNotifiable: boolean()
       .required()
-  })
+  }) 
