6c4b50ac439c15370bcfffb0bba0dd227365014e
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 11:58:20 -0300

    feat: tipagem para o AppointmentContextData

--- src/types/appointment-context-data.ts
+++ src/types/appointment-context-data.ts
@@ -3,10 +3,17 @@ import { AppointmentFormInput } from "./appointment-form-input";
 
 export interface AppointmentContextData {
   appointment: Appointment | undefined | null;
+  professionals: { label: string, value: string }[];
+  loading: boolean;
   createAppointment: (appointmentInput: AppointmentFormInput) => Promise<Appointment | undefined>;
+  updateAppointment: (id: string, appointmentInput: AppointmentFormInput) => Promise<Appointment | undefined>;
+  getServicesByProfessional: (professional_id: string) => Promise<{ label: string, value: string }[]>;
+  getAvailableHoursByProfessionalAndDate: (professional_id: string, date: string) => Promise<number[]>;
+  getService: (id: string) => Promise<Appointment>;
+  listAppointmentsByCustomer: () => Promise<Appointment[]>;
+  listAppointmentsByCustomerAndDate: (date: string) => Promise<Appointment[]>;
   // removeAppointment: (id: string) => Promise<boolean>;
   // listAppointments: () => Promise<Appointment[]>;
-  // listAppointmentsByCustomer: (customerId: string) => Promise<Appointment[]>;
   // listAppointmentsByProfessional: (professionalId: string) => Promise<Appointment[]>;
   // listAppointmentsByDate: (date: string) => Promise<Appointment[]>;
   // listAppointmentsByDateAndProfessional: (date: string, professionalId: string) => Promise<Appointment[]>;
