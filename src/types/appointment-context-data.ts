e381693d15a0ceeeb2feed6edbf16439fdb2027c
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 15:36:05 -0300

    feat: adiciona funcoes para o fluxo de reserva

--- src/types/appointment-context-data.ts
+++ src/types/appointment-context-data.ts
@@ -4,6 +4,7 @@ import { AppointmentFormInput } from "./appointment-form-input";
 export interface AppointmentContextData {
   appointment: Appointment | undefined | null;
   professionals: { label: string, value: string }[];
+  customers: { label: string, value: string }[];
   loading: boolean;
   createAppointment: (appointmentInput: AppointmentFormInput) => Promise<Appointment | undefined>;
   updateAppointment: (id: string, appointmentInput: AppointmentFormInput) => Promise<Appointment | undefined>;
@@ -12,9 +13,5 @@ export interface AppointmentContextData {
   getService: (id: string) => Promise<Appointment>;
   listAppointmentsByCustomer: () => Promise<Appointment[]>;
   listAppointmentsByCustomerAndDate: (date: string) => Promise<Appointment[]>;
-  // removeAppointment: (id: string) => Promise<boolean>;
-  // listAppointments: () => Promise<Appointment[]>;
-  // listAppointmentsByProfessional: (professionalId: string) => Promise<Appointment[]>;
-  // listAppointmentsByDate: (date: string) => Promise<Appointment[]>;
-  // listAppointmentsByDateAndProfessional: (date: string, professionalId: string) => Promise<Appointment[]>;
+  createReservation: (appointmentInput: AppointmentFormInput) => Promise<Appointment | undefined>;
 }
